e49a6c85cf9d1b2b467314af150ee1891b644dab
Fady Kuzman <fady.kuzman@codefuchs.dev>
2025-10-06 13:42:15 +0200

    Add keep and trash buttons
    updates interface and database

--- web/app.js
+++ web/app.js
@@ -29,20 +29,10 @@ async function showGroup(id) {
   imagesDiv.className = 'images-grid';
 
   files.forEach((file, index) => {
-    const fileDiv = document.createElement('div')
-    fileDiv.className = 'image-item';
-
-    fileDiv.innerHTML = `
-      <img src="/api/image?path=${encodeURIComponent(file.Path)}" alt="Image ${index + 1}">
-      <div class="metadata">
-        <div><strong>Path:</strong> ${file.Path}</div>
-        <div><strong>Size: </strong> ${formatBytes(file.Filesize)}</div>
-      </div>
-    `;
-
+    console.log(file)
+    const fileDiv = createImageDiv(file, index)
 
     imagesDiv.appendChild(fileDiv)
-
   });
 
   container.appendChild(imagesDiv);
@@ -52,6 +42,63 @@ async function showGroup(id) {
   backBtn.onclick = loadGroups;
   container.appendChild(backBtn);
 
+}
+
+function createImageDiv(file, index) {
+  const fileDiv = document.createElement('div')
+  fileDiv.className = 'image-item';
+
+  fileDiv.innerHTML = `
+    <div class="duplicate-image">
+      <img src="/api/image?path=${encodeURIComponent(file.Path)}" alt="Image ${index + 1}">
+      <div class="metadata">
+        <div><strong>Path:</strong> ${file.Path}</div>
+        <div><strong>Size: </strong> ${formatBytes(file.Filesize)}</div>
+      </div>
+    </div>
+      <div><button class="keep-button">Keep</button><button class="trash-button">Trash</button></div>
+    `;
+
+  const duplicateImage = fileDiv.querySelector(".duplicate-image")
+
+
+  const keepButton = fileDiv.querySelector(".keep-button");
+  keepButton.onclick = async () => {
+    duplicateImage.classList.remove("to-delete")
+    duplicateImage.classList.add("to-keep")
+
+    const response = await fetch(`/api/groups/${file.GroupID}/files/${file.ID}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        action: "keep"
+      })
+    })
+
+    console.log(response)
+
+  }
+
+  const trashButton = fileDiv.querySelector(".trash-button")
+  trashButton.onclick = async () => {
+    duplicateImage.classList.remove("to-keep")
+    duplicateImage.classList.add("to-delete")
+
+
+    const response = await fetch(`/api/groups/${file.GroupID}/files/${file.ID}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: "trash"
+      })
+    })
+  }
+
+  return fileDiv
 }
 
 function formatBytes(bytes) {
