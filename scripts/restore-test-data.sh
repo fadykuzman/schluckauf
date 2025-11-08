efea09220dbfdf7f1a4325c464f6fb3ae9cfc138
Fady Kuzman <fady.kuzman@codefuchs.dev>
2025-11-08 21:07:56 +0100

    Delete restore script

--- scripts/restore-test-data.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-PROJECT_DIR=${HOME}/Workspaces/Projects/schluckauf
-
-rm ${PROJECT_DIR}/data/duplicates.db
-
-cp -r ${PROJECT_DIR}/test-photos-original/* ${PROJECT_DIR}/test-photos/.
